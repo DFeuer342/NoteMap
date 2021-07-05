9530708630b3a2c61517fb82f68f419a408bd737
TiagoRebelo <tgr342r@gmail.com>
2021-07-05 21:34:51 +0100

    Actividade AddReporte

---
+++ app/src/main/java/ipvc/ecgm/NoteMap/AddReporte.kt
@@ -0,0 +1,11 @@
+package ipvc.ecgm.NoteMap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddReporte : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_reporte)
+    }
+}
